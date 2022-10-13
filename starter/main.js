3a2aa3b3f70f67fde85d091cf3190f2235a05f78
tertiux44 <secondsonofadebayo@gmail.com>
2022-10-13 13:44:53 +0100

    Added transition and some other cool stuff

---
+++ starter/main.js
@@ -0,0 +1,22 @@
+alert("Foodplug wants access to your call log 😈")
+alert("Foodplug wants access to your sms 😈")
+alert("Foodplug wants access to your notifications 🤪")
+
+var unavailablefeature = "This feature is not yet available 😀"
+
+function dosomething() {
+    alert(unavailablefeature)
+}
+
+
+
+
+
+console.log(hamburgerdiv)
+
+var hamburgerdiv = document.querySelector(".hamburger")
+
+
+function kaisen() {
+    hamburgerdiv.classList.toggle("showburger")
+}
