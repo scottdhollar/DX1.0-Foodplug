edc36f0961b949a057b5e7ec7dfa55b84c52d8d1
tertiux44 <secondsonofadebayo@gmail.com>
2022-11-21 17:51:00 +0100

    mobile version completed

--- starter/main.js
+++ starter/main.js
@@ -1,29 +1,29 @@
 // alert("Foodplug will have maximum access to your call log, sms and notifications 😈")
 
-ScrollReveal({ reset: true, duration: 2500, })
+ScrollReveal({ reset: true, duration: 2000, })
 
 
 // ScrollReveal().reveal('.gototop', {origin:'right', delay: 3000});
 // ScrollReveal().reveal('.mobile-logo', {origin:'left'});
-// ScrollReveal().reveal('.hamburger', {origin:'right'});
-// ScrollReveal().reveal('.counter', {origin:'right'});
-ScrollReveal().reveal('.footer-icon-holder a', {origin:'right'});
-// ScrollReveal().reveal('.foodcard-price', {origin:'right', delay: 1500});
+// ScrollReveal().reveal('.header-text-container', {origin:'left'});
+// ScrollReveal().reveal('.language', {origin:'right'});
+// ScrollReveal().reveal('.footer-icon-holder a', {origin:'right'});
+// ScrollReveal().reveal('.foodcard-price', {origin:'right', delay: 100});
 // ScrollReveal().reveal('.about-text-holder h2', {origin:'right'});
 
 var unavailablefeature = "This feature is not yet available, try again later 😀"
 
 
 
-function dosomething() {
-    alert(unavailablefeature)
-}
+// function dosomething() {
+//     alert(unavailablefeature)
+// }
 
 
-var contact= "https://wa.me/message/SQWCD3JZAO36B1"
-function callcharges() {
-    alert("🔔 Local call rates apply")
-}
+// var contact= "https://wa.me/message/SQWCD3JZAO36B1"
+// function callcharges() {
+//     alert("🔔 Local call rates apply")
+// }
 
 function contactus() {
     window.open (contact)
@@ -46,10 +46,19 @@ window.addEventListener('scroll', () =>{
     if (Math.ceil(scrolled) < scrollable){
         // alert('you\'ve reached the bottom')
         fabb.style.opacity = '0.8'
-    } else {
+        fabb.classList.remove("hidde")
+    } else if (Math.ceil(scrolled) === scrollable) {
         fabb.style.opacity = '0'
         console.log(scrollable)
-    }
+        document.querySelector('.box').classList.remove("box-active")
+        document.querySelector('.fab').classList.remove("fab-active")
+        fabb.classList.add("hidde")
+        } else {
+            fabb.style.opacity = '0'
+            document.querySelector('.box').classList.remove("box-active")
+            document.querySelector('.fab').classList.remove("fab-active")
+            fabb.classList.add("hidde")
+        }
 
     
 } )
@@ -68,6 +77,7 @@ function kaisen() {
     hamburgerdiv.classList.toggle("showburger")
     mobileLinks.classList.toggle("show-mobile-link-holder")
     mybackdrop.classList.toggle("show-backdrop")
+    mybackdrop.style.height = '100vh' 
     navm.classList.toggle("nav-mobilez")
     document.querySelector('.fab').classList.toggle("fab-kaisen")
     document.querySelector('.box').classList.remove("box-active")
