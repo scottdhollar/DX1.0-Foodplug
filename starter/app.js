be951cb744e4afec51a7f1a3d684958146ac1156
tertiux44 <secondsonofadebayo@gmail.com>
2022-12-26 22:37:41 +0100

    final desktop changes

---
+++ starter/app.js
@@ -0,0 +1,81 @@
+function helen(){
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            console.log(entry)
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                // entry.target.classList.remove('show')
+            }
+        });
+    },
+    {
+        threshold: 0.5,
+        // rootMargin: "-100px"
+    }
+    
+    
+    );
+    
+    
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
+    
+    const observerii = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            console.log(entry)
+            if (entry.isIntersecting) {
+                entry.target.classList.add('hidegototop');
+                document.querySelector('.gototop').classList.remove('hideme')
+            } else {
+                entry.target.classList.remove('hidegototop')
+                document.querySelector('.gototop').classList.add('hideme')
+            }
+        });
+    });
+    
+    
+    const hiddenElementsii = document.querySelectorAll('.navm');
+    hiddenElementsii.forEach((el) => observerii.observe(el));
+
+}
+
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         console.log(entry)
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('show');
+//         } else {
+//             // entry.target.classList.remove('show')
+//         }
+//     });
+// },
+// {
+//     threshold: 0.5,
+//     // rootMargin: "-100px"
+// }
+
+
+// );
+
+
+// const hiddenElements = document.querySelectorAll('.hidden');
+// hiddenElements.forEach((el) => observer.observe(el));
+
+// const observerii = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         console.log(entry)
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('hidegototop');
+//             document.querySelector('.gototop').classList.remove('hideme')
+//         } else {
+//             entry.target.classList.remove('hidegototop')
+//             document.querySelector('.gototop').classList.add('hideme')
+//         }
+//     });
+// });
+
+
+// const hiddenElementsii = document.querySelectorAll('.navm');
+// hiddenElementsii.forEach((el) => observerii.observe(el));
+
